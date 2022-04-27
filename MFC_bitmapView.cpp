0bef9fc5330750f22f94357c24b74e6f01e296da
동혁 박 <jay5713665@gmai.com>
2022-04-28 01:01:31 +0900

    초기 커밋.

---
+++ MFC_bitmapView.cpp
@@ -0,0 +1,124 @@
+﻿
+// MFC_bitmapView.cpp: CMFCbitmapView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFC_bitmap.h"
+#endif
+
+#include "MFC_bitmapDoc.h"
+#include "MFC_bitmapView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCbitmapView
+
+IMPLEMENT_DYNCREATE(CMFCbitmapView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCbitmapView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFCbitmapView 생성/소멸
+
+CMFCbitmapView::CMFCbitmapView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCbitmapView::~CMFCbitmapView()
+{
+}
+
+BOOL CMFCbitmapView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCbitmapView 그리기
+
+void CMFCbitmapView::OnDraw(CDC* pDC)
+{
+	CMFCbitmapDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+
+
+	Graphics g(pDC->m_hDC);
+	Image image(pDoc->m_ImgPath);
+
+
+	int width = 100;
+	int height = 100;
+
+	g.DrawImage(&image,10,10,200,200);
+	g.DrawImage(&image,210,10,200,200);
+
+	image.RotateFlip(Rotate90FlipNone);
+
+	g.DrawImage(&image,410,10,200,200);
+
+	
+
+
+}
+
+
+// CMFCbitmapView 인쇄
+
+BOOL CMFCbitmapView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCbitmapView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CMFCbitmapView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CMFCbitmapView 진단
+
+#ifdef _DEBUG
+void CMFCbitmapView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCbitmapView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCbitmapDoc* CMFCbitmapView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCbitmapDoc)));
+	return (CMFCbitmapDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCbitmapView 메시지 처리기
---
+++ MFC_bitmapView.h
@@ -0,0 +1,49 @@
+﻿
+// MFC_bitmapView.h: CMFCbitmapView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMFCbitmapView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CMFCbitmapView() noexcept;
+	DECLARE_DYNCREATE(CMFCbitmapView)
+
+// 특성입니다.
+public:
+	CMFCbitmapDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CMFCbitmapView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFC_bitmapView.cpp의 디버그 버전
+inline CMFCbitmapDoc* CMFCbitmapView::GetDocument() const
+   { return reinterpret_cast<CMFCbitmapDoc*>(m_pDocument); }
+#endif
+
